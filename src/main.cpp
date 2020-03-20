366562e2d53c5d0e2f5a31b1052685e0cc391e3d
James Liu <james_liu@fastmail.com>
2020-03-20 20:37:14 +1100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,19 @@
 
 extern "C" int main()
 {
-  // Switch on blue LED on STM32F407Discovery
-
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
   GPIO_InitTypeDef gpio_out = {
-    .GPIO_Pin = GPIO_Pin_15,
+    .GPIO_Pin = GPIO_Pin_5,
     .GPIO_Mode = GPIO_Mode_OUT,
     .GPIO_Speed = GPIO_High_Speed,
     .GPIO_OType = GPIO_OType_PP,
     .GPIO_PuPd = GPIO_PuPd_NOPULL
   };
 
-  GPIO_Init(GPIOD, &gpio_out);
-  GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_SET);
+  GPIO_Init(GPIOA, &gpio_out);
 
-  for (;;);
+  GPIO_ToggleBits(GPIOA, GPIO_Pin_5);
 
-  return 0;
+  for (;;){}
 }
